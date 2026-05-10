3ee9b3b229afdca19a36a48004a40c1259309527
nandakishore-dasari <nandudasari07@gmail.com>
2026-05-10 23:23:13 +0530

    Fixed render port

--- index.js
+++ index.js
@@ -194,9 +194,10 @@ ${context}
 // ========================================
 // SERVER
 // ========================================
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-  console.log("Server Running On:");
-  console.log("http://localhost:3000");
+app.listen(PORT, () => {
+
+  console.log(`Server Running On Port ${PORT}`);
 
 });
